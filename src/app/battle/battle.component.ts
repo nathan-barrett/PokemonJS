c63692c0e06b9d8ded2b86d64a527c8f3518d802
Amy Churchwell and Nathan Barrett <amy@>
2018-01-29 15:35:36 -0800

    added even more styles to the battle menu

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-battle',
   templateUrl: './battle.component.html',
-  styleUrls: ['./battle.component.css']
+  styleUrls: ['./battle.component.scss']
 })
 export class BattleComponent implements OnInit {
 
