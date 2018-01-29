503de079bd0d44bee166d4044f2cdae25268e355
Luke Bertram and Lew Davidson <lew@>
2018-01-29 13:28:30 -0800

    successfully got the canvas to draw

--- src/app/map.model.ts
+++ src/app/map.model.ts
@@ -1,10 +1,15 @@
 export class Map {
 
   constructor(
-    public tileSize: number,
+    public tileSize: number, //dimension of square tile in px
     public atlas: string, //path to tileset image?
-    public cols: number,
-    public rows: number,
-    public tileGrid: number[]
+    public columns: number, //number of columns in the map grid
+    public rows: number, //number of rows in the map grid
+    public tileGrid: number[] //stores a numerical value for each tile in the grid
     ){}
+
+  //input coordinates and recieve the numerical representation of that tile
+  getTile(col: number, row: number){
+    return this.tileGrid[row * this.columns + col];
+  }
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, Directive, ElementRef } from '@angular/core';
+import { Map } from '../map.model';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  @ViewChild('areaMap') areaMap: ElementRef;
+
+  map = new Map(
+    64, //tile size of 64px
+    'location of tilset image',
+    10, //number of cols in the map
+    10, //number of rows in the map
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    ]
+  );
+
   constructor() { }
 
   ngOnInit() {
+    let ctx: CanvasRenderingContext2D = this.areaMap.nativeElement.getContext('2d');
+
+    ctx.clearRect(0, 0, 640, 640);
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(10, 12, 50, 64);
+  }
+
+  drawStuff(){
+    let ctx: CanvasRenderingContext2D = this.areaMap.nativeElement.getContext('2d');
+
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(0, 0, 640, 640);
   }
 
 }
