b774716cc15868a8353b366ab49d499f6b3c6a29
luke bertram <lukebertram@gmail.com>
2018-01-30 13:36:27 -0800

    add keyboard model with method stubs

---
+++ src/app/keyboard.model.ts
@@ -0,0 +1,24 @@
+export class Keyboard {
+  LEFT = 37;
+  RIGHT = 39;
+  UP = 38;
+  DOWN = 40;
+
+  _keys = {};
+
+  listenForEvents(keys) {
+
+  }
+
+  _onKeyDown(event) {
+
+  }
+
+  _onKeyUp(event) {
+
+  }
+
+  isDown(keyCode) {
+    
+  }
+}
