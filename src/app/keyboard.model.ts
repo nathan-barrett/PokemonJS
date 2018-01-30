6c436b7f30f4bec139e70536112b7f9bf1ef0b5a
luke bertram <lukebertram@gmail.com>
2018-01-30 14:10:36 -0800

    fill in keyboard.model from tilemap-reference

--- src/app/keyboard.model.ts
+++ src/app/keyboard.model.ts
@@ -7,18 +7,34 @@ export class Keyboard {
   _keys = {};
 
   listenForEvents(keys) {
+    window.addEventListener('keydown', this._onKeyDown.bind(this));
+    window.addEventListener('keyup', this._onKeyUp.bind(this));
 
+    keys.forEach(function (key) {
+        this._keys[key] = false;
+    }.bind(this));
   }
 
   _onKeyDown(event) {
-
+    const keyCode = event.keyCode;
+    if (keyCode in this._keys) {
+      event.preventDefault();
+      this._keys[keyCode] = true;
+    }
   }
 
   _onKeyUp(event) {
-
+    const keyCode = event.keyCode;
+    if (keyCode in this._keys) {
+      event.preventDefault();
+      this._keys[keyCode] = false;
+    }
   }
 
-  isDown(keyCode) {
-    
+  isDown(keyCode: number) {
+    if (!(keyCode in this._keys)) {
+      throw new Error(`Keycode ${keyCode} is not being listened to`);
+    }
+    return this._keys[keyCode];
   }
 }
